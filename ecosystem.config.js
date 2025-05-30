1f763bf42d53422656b15168f5595ff1dcffe1ad
Mohanprasath6469 <154488458+Mohanprasath6469@users.noreply.github.com>
2025-05-30 12:29:19 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  apps: [
-    {
-      name: 'testing_project',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
-      cwd: '/home/code/deploy',
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
-  ],
+  apps: [{
+    name: 'ePoll-Audit',
+    script: 'npm',
+    args: 'start',
+    cwd: './',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
 };
